5438df446875d3c766cb1372f17e3eabe0facd40
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2023-12-26 15:57:57 +0100

    Update to Ex 1.2:  Array of Objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world!');
-const favoriteFood = "chicken";
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList[0] = {
+    name:   'Bulbasaur' ,
+    height: 0.7,
+    types:  ['grass', 'poison']
+};
+
+pokemonList[1] = {
+    name:   'Charmander' ,
+    height: 0.6,
+    types:  ['fire']
+};
+
+pokemonList[2] = {
+    name:   'Squirtle ' ,
+    height: 0.5,
+    types:  ['water']
+};
+
+console.log(pokemonList)
