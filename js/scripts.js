2e33db172d07cf73524c8237ea5bf8179134a09d
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2023-12-30 19:19:43 +0100

    Update colors in styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,85 @@
-let pokemonList = [];
+let pokemonRepository = (
+    function () 
+    {
+        let pokemonList = [];
 
-pokemonList[0] = {
-    name:   "Bulbasaur" ,
-    height: 0.7,
-    types:  ["grass", "poison"]
-};
+        // Prototype of the pokemon object for easy comparison
+        const pokemonPrototype = {
+            name: "",
+            height: "",
+            types: []
+        }
 
-pokemonList[1] = {
-    name:   "Charmander" ,
-    height: 0.6,
-    types:  ["fire"]
-};
+        // pokemon added by hand 
+        pokemonList[0] = {
+            name:   "Bulbasaur" ,
+            height: 0.7,
+            types:  ["grass", "poison"]
+        };
+        
+        pokemonList[1] = {
+            name:   "Charmander" ,
+            height: 0.6,
+            types:  ["fire"]
+        };
+        
+        pokemonList[2] = {
+            name:   "Squirtle " ,
+            height: 0.5,
+            types:  ["water"]
+        };
+        
+        // function for comparing two arrays
+        const compareArrays = (a, b) =>
+         a.length === b.length && a.every((element, index) => element === b[index]);
 
-pokemonList[2] = {
-    name:   "Squirtle " ,
-    height: 0.5,
-    types:  ["water"]
-};
+        // function for adding a pokemon object to pokemonList
+        function add(pokemon) {
+            // Check if pokemon is indeed an object
+            if(typeof pokemon === "object")
+            {
+                // check if pokemon-object has the correct keys
+                if( compareArrays(Object.keys(pokemon), Object.keys(pokemonPrototype)) )
+                {
+                    pokemonList.push(pokemon) 
+                }        
+            } 
+        }
+        
+        // return the whole list of pokemon
+        function getAll() {
+            return pokemonList;
+        }
+    
+        return {
+            add: add,
+            getAll: getAll
+        };
+    }
+)();
 
-//cache variable
-let pokemonProps = ""
+// adding a pokemon to the list
+pokemonRepository.add({name: "Pikachu", height: 0.4, types: ["Electro"]});
+
+
 
 //get div container
 const container = document.getElementById("pokemon--container");
 
 // iterate over pokemonList
-for(i=0; i < pokemonList.length; i++)
-{   
+pokemonRepository.getAll().forEach( (pokemon) => {
+   
     // save name and height in cache-variable
-    pokemonProps = pokemonList[i].name + " ( height: " + pokemonList[i].height + " )";  
+    let pokemonProps = pokemon.name + " ( height: " + pokemon.height + " )";  
 
     // conditional
-    if(pokemonList[i].height >= 0.6)
+    if(pokemon.height >= 0.6)
     {
         pokemonProps += " - Wow, that's big!"     
     }
 
     // insert div element with pokemon info into container
-     container.innerHTML += `<div class="pokemon"> ${pokemonProps} </div>`; 
-}
+    container.innerHTML += `<div class="pokemon"> ${pokemonProps} </div>`; 
+    }
+)  
+   
